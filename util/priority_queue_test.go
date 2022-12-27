d2e76f8bcd37e085a7b0b1cda4654d111976d56c
奇葩の灵梦 <415551921@qq.com>
2022-12-27 20:24:35 +0800

    增加测试用例

--- util/priority_queue_test.go
+++ util/priority_queue_test.go
@@ -2,9 +2,12 @@ package util
 
 import (
 	"fmt"
+	"golang.org/x/exp/slices"
+	"math/rand"
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestCreatorAndPoll(t *testing.T) {
@@ -17,6 +20,27 @@ func TestCreatorAndPoll(t *testing.T) {
 	if s != "3 4 5 6 9" {
 		t.Log("incorrect Foreach: ", s, "| 3 4 5 6 9")
 		t.Fail()
+	}
+}
+
+func TestCreatorAndPoll2(t *testing.T) {
+	r := rand.New(rand.NewSource(time.Now().UnixMilli()))
+	for i := 0; i < 100; i++ {
+		s := make([]int, 0, 100)
+		for j := 0; j < 100; j++ {
+			s = append(s, r.Intn(99999))
+		}
+		q := NewPriorityQueue(append([]int(nil), s...), func(o1, o2 int) int { return o1 - o2 })
+		s1 := make([]int, 0, 100)
+		for q.Len() > 0 {
+			s1 = append(s1, q.Poll())
+		}
+		s2 := append([]int(nil), s...)
+		slices.Sort(s2)
+		if !slices.Equal(s1, s2) {
+			t.Log("incorrect order: ", s)
+			t.Fail()
+		}
 	}
 }
 
@@ -37,5 +61,30 @@ func TestAddAndPoll(t *testing.T) {
 	if s != "3 4 5 6 9" {
 		t.Log("incorrect Foreach: ", s, "| 3 4 5 6 9")
 		t.Fail()
+	}
+}
+
+func TestAddAndPoll2(t *testing.T) {
+	r := rand.New(rand.NewSource(time.Now().UnixMilli()))
+	for i := 0; i < 100; i++ {
+		q := &PriorityQueue[int]{
+			Comparator: func(o1, o2 int) int { return o1 - o2 },
+		}
+		s := make([]int, 0, 100)
+		for j := 0; j < 100; j++ {
+			v := r.Intn(99999)
+			s = append(s, v)
+			q.Add(v)
+		}
+		s1 := make([]int, 0, 100)
+		for q.Len() > 0 {
+			s1 = append(s1, q.Poll())
+		}
+		s2 := append([]int(nil), s...)
+		slices.Sort(s2)
+		if !slices.Equal(s1, s2) {
+			t.Log("incorrect order: ", s)
+			t.Fail()
+		}
 	}
 }
