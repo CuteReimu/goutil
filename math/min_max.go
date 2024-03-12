0f51b214e0253dd8093af38f37c898c431ce61a0
奇葩の灵梦 <415551921@qq.com>
2024-03-12 12:00:24 +0800

    Update min_max.go

--- math/min_max.go
+++ math/min_max.go
@@ -5,6 +5,10 @@ import (
 )
 
 // Max 返回多个数中最大的那个
+//
+// deprecated
+//
+// 已废弃，请直接使用 max(1, 2)
 func Max[T cmp.Ordered](nums ...T) T {
 	m := nums[0]
 	for i := 1; i < len(nums); i++ {
@@ -16,6 +20,10 @@ func Max[T cmp.Ordered](nums ...T) T {
 }
 
 // Min 返回多个数中最小的那个
+//
+// deprecated
+//
+// 已废弃，请直接使用 min(1, 2)
 func Min[T cmp.Ordered](nums ...T) T {
 	m := nums[0]
 	for i := 1; i < len(nums); i++ {
